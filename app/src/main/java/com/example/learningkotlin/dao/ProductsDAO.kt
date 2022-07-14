3a73d5efe6e1ff4cb39137304973f0cef10204e6
arttandrade <adsa@icomp.ufam.edu.br>
2022-07-13 21:34:00 -0400

    Add a default Product

--- app/src/main/java/com/example/learningkotlin/dao/ProductsDAO.kt
+++ app/src/main/java/com/example/learningkotlin/dao/ProductsDAO.kt
@@ -1,11 +1,12 @@
 package com.example.learningkotlin.dao
 
 import com.example.learningkotlin.model.Product
+import java.math.BigDecimal
 
 open class ProductsDAO {
 
     internal companion object {
-        private var list: ArrayList<Product> = ArrayList<Product>()
+        private var list = mutableListOf<Product>(Product(name = "Salada de Frutas", description = "Laranja, maça e uva", value = BigDecimal("5.00")))
     }
 
 
